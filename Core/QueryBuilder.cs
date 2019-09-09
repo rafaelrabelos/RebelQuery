a3e56da3415f215a85cddb02cd5346f9126be7e8
Rafael A. Rabelo <rafael_rabelo@live.com>
2019-09-09 17:46:48 -0300

    Fixed query builder unsupported null arguments

--- Core/QueryBuilder.cs
+++ Core/QueryBuilder.cs
@@ -3,7 +3,6 @@ using System.Reflection;
 using System.Text;
 using System.Linq;
 
-
 namespace RebelQuery.Core
 {
     using Models;
@@ -15,18 +14,18 @@ namespace RebelQuery.Core
     {
         public object SelectArgs {get; set;}
         public object WhereArgs {get; set;}
-        protected SqlQuery BuildAnQuery(DQL command)
+
+        protected SqlQuery BuildAnQuery(string queryStr, object args =null)
         {
-            
-            
-            SqlQuery query = new SqlQuery
-            {
-                CommandName = command.ToString(),
-                TableName = this.GetType().Name,
-            };
+            this.QueryString = queryStr;
+
+            return this;
+        }
+        protected SqlQuery BuildAnQuery(DQL command, object args =null)
+        {
             this.QueryString = new StringBuilder("{command} {what} FROM {table} {Where}")
-            .Replace("{command}",query.CommandName)
-            .Replace("{table}",query.TableName)
+            .Replace("{command}", command.ToString())
+            .Replace("{table}", this.GetType().Name)
             .Replace("{what}", BuildSelectArgs())
             .Replace("{Where}", BuildWhereArgs())
             .ToString();
@@ -34,14 +33,14 @@ namespace RebelQuery.Core
             return this;
         }
 
-        protected SqlQuery BuildAnQuery(DDL command)
+        protected SqlQuery BuildAnQuery(DDL command, object args =null)
         {
             return new SqlQuery{
                 QueryString = command.ToString()
             };
         }
 
-        protected SqlQuery BuildAnQuery(DML command)
+        protected SqlQuery BuildAnQuery(DML command, object args =null)
         {
             return new SqlQuery{
                QueryString = command.ToString()
@@ -50,9 +49,13 @@ namespace RebelQuery.Core
 
         private string  BuildSelectArgs()
         {
-            if( this.SelectArgs == null ){
+            if( this.SelectArgs == null 
+            || object.Equals(this.SelectArgs, new{}) 
+            || this.SelectArgs.GetType().GetProperties().Count() <= 0 
+            ){
                 return "*";
             }
+
             return  string.Join(
                     ", ", this.SelectArgs
                     .GetType()
@@ -64,9 +67,13 @@ namespace RebelQuery.Core
 
         private string  BuildWhereArgs()
         {
-            if( this.WhereArgs == null ){
+            if( this.WhereArgs == null 
+            || object.Equals(this.SelectArgs, new{}) 
+            || this.SelectArgs.GetType().GetProperties().Count() <= 0 
+            ){
                 return String.Empty;
             }
+            
             return "WHERE " + string.Join(
                     " ", this.WhereArgs
                     .GetType()
